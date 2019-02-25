3bfa8cac35e6c31d49e42e1ae5cd34876243527c
suriarasai <suria.r.asai@gmail.com>
2019-02-25 01:27:16 -0800

    stock example

---
+++ W04-Statistics/src/simple/SparkContextExample.scala
@@ -0,0 +1,15 @@
+package simple
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object SparkContextExample {
+   def main(args: Array[String]) {
+    val stocksPath = "hdfs://quickstart.cloudera/user/cloudera/stocks.txt"
+    val conf = new SparkConf().setAppName("Counting Lines").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile(stocksPath, 2)
+    val totalLines = data.count()
+    println("Total number of Lines: %s".format(totalLines))
+  }
+}
