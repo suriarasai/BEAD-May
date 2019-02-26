c4d35a82a52f2015f0fb418556cb170541e984c6
suriarasai <suria.r.asai@gmail.com>
2019-02-25 20:36:22 -0800

    ML W06

---
+++ W06-SparkML/src/regressionexample/LinearRegression_Variable_Identification.scala
@@ -0,0 +1,35 @@
+package regressionexample
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.DoubleType
+object LinearRegression_VariableIdentification {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setMaster("local[2]")
+      .setAppName("Sales_Prediction")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+    val sqlContext = new SQLContext(sc)
+    
+    //Loading data
+    val sales_data_train =
+      sqlContext.read.format("com.databricks.spark.csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load("hdfs://quickstart.cloudera/user/cloudera/Sales_Train.csv")
+    val sales_data_test =
+      sqlContext.read.format("com.databricks.spark.csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load("hdfs://quickstart.cloudera/user/cloudera/Sales_Test.csv")
+    val sales_data_union = sales_data_train.unionAll(sales_data_test)
+    val sales_data = sales_data_union.withColumn(
+      "Item_Outlet_Sales",
+      sales_data_union.col("Item_Outlet_Sales").cast(DoubleType))
+    sales_data.show(5)
+  }
+
+}
